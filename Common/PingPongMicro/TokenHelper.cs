db45fdaf53b5285ff8ae66f08fb95fcdc65f93cc
Junikiewicz <32637113+Junikiewicz@users.noreply.github.com>
2020-04-10 15:24:17 +0200

    Nuget repo and source for nuget package

---
+++ Common/PingPongMicro/TokenHelper.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace PingPongMicro
+{
+    public class TokenHelper
+    {
+        public static TokenValidationParameters GetTokenValidationParameters(SecurityKey key)
+        {
+            return  new TokenValidationParameters
+            {
+                IssuerSigningKey = key,
+                RequireSignedTokens = true,
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+                ValidateAudience = false,
+                ValidateActor = false,
+                ValidateIssuer = false
+            };
+        }
+        public static bool ValidateTokenManualy(string token, string publicKey)
+        {
+            SecurityKey key = BuildRsaSigningKey(publicKey);
+
+            var validationParameters = GetTokenValidationParameters(key);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            ClaimsPrincipal principal;
+            try
+            {
+                principal = tokenHandler.ValidateToken(token, validationParameters, out var rawValidatedToken);
+                var securityToken = (JwtSecurityToken)rawValidatedToken;
+            }
+            catch (Exception ex)
+            {
+                if (ex is SecurityTokenInvalidSignatureException || ex is ArgumentException || ex is SecurityTokenExpiredException)
+                {
+                    return false;
+                }
+                throw;
+            }
+            return principal.Identity.IsAuthenticated;
+        }
+        public static RsaSecurityKey BuildRsaSigningKey(string xml)
+        {
+            var parameters = ParseXmlString(xml);
+            var rsaProvider = new RSACryptoServiceProvider(2048);
+            rsaProvider.ImportParameters(parameters);
+            var key = new RsaSecurityKey(rsaProvider);
+            return key;
+        }
+        private static RSAParameters ParseXmlString(string xml)
+        {
+            RSAParameters parameters = new RSAParameters();
+
+            System.Xml.XmlDocument xmlDoc = new System.Xml.XmlDocument();
+            xmlDoc.LoadXml(xml);
+
+            if (xmlDoc.DocumentElement.Name.Equals("RSAKeyValue"))
+            {
+                foreach (System.Xml.XmlNode node in xmlDoc.DocumentElement.ChildNodes)
+                {
+                    switch (node.Name)
+                    {
+                        case "Modulus": parameters.Modulus = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "Exponent": parameters.Exponent = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "P": parameters.P = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "Q": parameters.Q = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "DP": parameters.DP = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "DQ": parameters.DQ = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "InverseQ": parameters.InverseQ = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "D": parameters.D = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid XML RSA key.");
+            }
+            return parameters;
+        }
+        
+    }
+}
