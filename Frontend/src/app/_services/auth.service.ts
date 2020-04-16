be5eb97ee5fcef9087514781d41ede6bed1f629b
Junikiewicz <32637113+Junikiewicz@users.noreply.github.com>
2020-04-16 18:21:17 +0200

    Auth service for login, register and token validation

---
+++ Frontend/src/app/_services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  gatewayUrl = environment.gatewayUrl;
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
+
+  constructor(private http: HttpClient) {}
+
+  login(model: any) {
+    return this.http.post(this.gatewayUrl + 'login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        }
+      })
+    );
+  }
+  register(model: any) {
+    return this.http.post(this.gatewayUrl + 'register', model);
+  }
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    if (this.jwtHelper.isTokenExpired(token)) {
+      return false;
+    }
+    return true;
+  }
+}
