ba0eefdc1f543321107d21b981ab348202637d1b
patryk1526 <patryk1526@gmail.com>
2020-04-23 18:33:24 +0200

    Add dtos

---
+++ Services/Room_Service/Dtos/PingPongTableForUpdateDto.cs
@@ -0,0 +1,15 @@
+namespace Room_Service.Dtos
+{
+    public class PingPongTableForUpdateDto
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public double TableLength { get; set; }
+        public double TableWidth { get; set; }
+        public bool VerticalOrientation { get; set; }
+        public int RoomId { get; set; }
+        public bool AvailableForReservations { get; set; }
+    }
+}
