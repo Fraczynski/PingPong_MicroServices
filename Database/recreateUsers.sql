240ea0e820ad9350d617443c755830e6e74314b8
patryk1526 <patryk1526@gmail.com>
2020-04-23 21:39:59 +0200

    Update sql files

--- Database/recreateUsers.sql
+++ Database/recreateUsers.sql
@@ -7,3 +7,9 @@ GRANT ALL PRIVILEGES ON pingpongapp.aspnetusers TO 'authService'@'localhost';
 GRANT ALL PRIVILEGES ON pingpongapp.aspnetusertokens TO 'authService'@'localhost';
 GRANT ALL PRIVILEGES ON pingpongapp.__efmigrationshistory TO 'authService'@'localhost';
 FLUSH PRIVILEGES;
+
+CREATE USER 'roomsService'@'localhost' IDENTIFIED BY 'roomsServicePassword';
+GRANT ALL PRIVILEGES ON pingpongapp.rooms TO 'roomsService'@'localhost';
+GRANT ALL PRIVILEGES ON pingpongapp.tables TO 'roomsService'@'localhost';
+GRANT ALL PRIVILEGES ON pingpongapp.__efmigrationshistory TO 'roomsService'@'localhost';
+FLUSH PRIVILEGES;
