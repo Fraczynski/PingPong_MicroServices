315511b301225c7c2c0c72e408b4899d60bdd021
Junikiewicz <32637113+Junikiewicz@users.noreply.github.com>
2020-05-01 13:54:23 +0200

    Added missing rooms-view dependencies, imports and services.

---
+++ Frontend/src/app/_models/table.ts
@@ -0,0 +1,10 @@
+interface Table {
+  id: number;
+  label: string;
+  x: number;
+  y: number;
+  tableLength: number;
+  tableWidth: number;
+  verticalOrientation: boolean;
+  roomId: number;
+}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -9,43 +9,47 @@ import { AppRoutingModule } from './app-routing.module';
 
 // ngx-bootstrap
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { defineLocale, plLocale } from 'ngx-bootstrap/chronos';
 
 // components
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { RoomsViewComponent } from './rooms-view/rooms-view.component';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
 }
+defineLocale('pl', plLocale);
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavMenuComponent,
-      RegisterComponent,
-      HomeComponent,
-	  RoomsViewComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      BrowserAnimationsModule,
-      AppRoutingModule,
-      FormsModule,
-	  BsDatepickerModule.forRoot(),
-      JwtModule.forRoot({
-         config: {
-           tokenGetter,
-           whitelistedDomains: ['localhost:5000'],
-           blacklistedRoutes: ['localhost:5000/api/auth']
-         }
-       }),
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    RegisterComponent,
+    HomeComponent,
+    RoomsViewComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter,
+        whitelistedDomains: ['localhost:5100'],
+        blacklistedRoutes: ['localhost:5100/api/auth'],
+      },
+    }),
     // ngx-bootstrap
     BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
