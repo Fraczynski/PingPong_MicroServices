ae1f8c0c35c37c97292aa4a35aa258d4e70639af
Junikiewicz <32637113+Junikiewicz@users.noreply.github.com>
2020-05-03 23:00:46 +0200

    pagination helper classes

---
+++ Services/Reservation_Service/Helpers/ReservationParams.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Reservation_Service.Helpers
+{
+    public class ReservationParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int pageSize = 10;
+        public int PageSize
+        {
+            get { return pageSize; }
+            set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+        public DateTime Start { get; set; }
+        public int? UserId { get; set; }
+        public string PingPongTableLabel { get; set; }
+        public string OrderBy { get; set; }
+    }
+}
