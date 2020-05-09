ec11462b13f2d5684b6c0da1a815d28759bd195c
Junikiewicz <mjunikiewicz@gmail.com>
2020-05-09 23:33:22 +0200

    reservation service authorization

--- Services/Reservation_Service/Startup.cs
+++ Services/Reservation_Service/Startup.cs
@@ -9,10 +9,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Reservation_Service.Data;
 using AutoMapper;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace Reservation_Service
 {
@@ -29,9 +32,23 @@ namespace Reservation_Service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-             services.AddScoped<IReservationRepository, ReservationRepository>();
-            services.AddDbContext<DataContext>(x=>x.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IReservationRepository, ReservationRepository>();
+            services.AddDbContext<DataContext>(x => x.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddAutoMapper(typeof(ReservationRepository).Assembly);
+            string publicAuthorizationKey;
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = httpClient.GetAsync("http://localhost:5100/api/auth/publickey"))
+                {
+                    publicAuthorizationKey = response.Result.Content.ReadAsStringAsync().Result;
+                }
+            }
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+                options =>
+                {
+                    var key = PingPongMicro.TokenHelper.BuildRsaSigningKey(publicAuthorizationKey);
+                    options.TokenValidationParameters = PingPongMicro.TokenHelper.GetTokenValidationParameters(key);
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +63,7 @@ namespace Reservation_Service
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
