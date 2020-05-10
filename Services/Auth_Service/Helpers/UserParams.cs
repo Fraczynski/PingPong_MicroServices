e0e31bbca3b762ec606fd771bfac1b5dec21374d
Junikiewicz <mjunikiewicz@gmail.com>
2020-05-10 14:12:10 +0200

    User pagination,sorting,ordering helpers

---
+++ Services/Auth_Service/Helpers/UserParams.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Auth_Service.Helpers
+{
+     public class UserParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int pageSize = 10;
+        public int PageSize
+        {
+            get { return pageSize; }
+            set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+        public Nullable<int> Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role{get;set;}
+        public string Email{get;set;}
+        public string OrderBy { get; set; }
+    }
+}
