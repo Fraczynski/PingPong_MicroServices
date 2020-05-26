d2fab165614049b8ed783d0f71dff60b6aa62f4d
Frączyński <patryk1526@gmail.com>
2020-05-26 14:49:46 +0200

    Reports - create controller

---
+++ Services/Reservation_Service/Controllers/ReportsController.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Reservation_Service.Data;
+using Reservation_Service.Dtos;
+using Reservation_Service.Models;
+
+namespace Reports_Service.Controllers
+{
+    [Authorize(Roles = "Employee")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IReportsRepository _repository;
+
+        public ReportsController(IReportsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetReport(ReportType type, [FromQuery] ReportParams reportParams)
+        {
+            var report = _repository.GetReport(type, reportParams);
+
+            return Ok(report);
+        }
+    }
+}
